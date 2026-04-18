2f162f7648019eb7da003284ca60258f6d16aa7d
audith <audithadhikary081@gmail.com>
2026-04-18 18:56:51 +0600

    token add

---
+++ app/auth.py
@@ -0,0 +1,24 @@
+from datetime import datetime,timedelta
+from passlib.context import CryptContext
+from jose import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY=os.getenv("SECRET_KEY")
+ALGORITHM="HS256"
+
+pwd_context=CryptContext(schemes=["argon2"],depecated="auto")
+
+
+def hash_password(password:str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password:str,hashed_password:str):
+    return pwd_context.verify(plain_password,hashed_password)
+
+def create_token(data:dict):
+    to_encode=data.copy()
+    to_encode.update({"exp":datetime.utcnow()+timedelta(minutes=60)})
+    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
