63da06a2100ed28765985b90227c56e7ae347523
audith <audithadhikary081@gmail.com>
2026-04-18 19:58:19 +0600

    student router

---
+++ app/routes/student.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+from database import SessionLocal
+import crud,schemas
+
+
+router = APIRouter(prefix="/students")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/")
+def add_student(student: schemas.StudentBase, db=Depends(get_db)):
+    return crud.create_student(db, student)
+
+@router.get("/")
+def list_students(db=Depends(get_db)):
+    return crud.get_students(db)
+
+@router.get("/{student_id}")
+def remove_student(student_id: int, db=Depends(get_db)):
+    return crud.delete_student(db, student_id)
